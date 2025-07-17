b93b70d4009cba9f5c0950eafbe10455844255ce
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-07-17 10:00:01 +0530

    router setup

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
-package main 
+package main
 
-func main (){
-	
+import (
+	"context"
+	"net/http"
+)
+
+func main() {
+	rootCtx := context.Background()
+	ctx, cancel := context.WithCancel(rootCtx)
+
+	defer cancel()
+
+	manager := NewManager(ctx)
+
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serverWS)
+
 }
