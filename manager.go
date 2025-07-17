89e9c5e2597050d023d8e196f79544a07226c882
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-07-17 10:10:46 +0530

    add remove client

--- manager.go
+++ manager.go
@@ -11,14 +11,13 @@ import (
 
 var (
 	wsUpgrader = websocket.Upgrader{
-		CheckOrigin : checkOrigin,
+		CheckOrigin:     checkOrigin,
 		WriteBufferSize: 1024,
-		ReadBufferSize: 1024,
+		ReadBufferSize:  1024,
 	}
 )
 
-
-func checkOrigin (r *http.Request) bool {
+func checkOrigin(r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
 	switch origin {
@@ -33,17 +32,16 @@ type Manager struct {
 	clients ClientList
 
 	sync.RWMutex
-
 }
 
-func NewManager(ctx context.Context) *Manager{
+func NewManager(ctx context.Context) *Manager {
 	m := &Manager{
-		clients:  make(ClientList),
+		clients: make(ClientList),
 	}
 	return m
 }
 
-func (m* Manager) serverWS(w http.ResponseWriter, r *http.Request){
+func (m *Manager) serverWS(w http.ResponseWriter, r *http.Request) {
 	log.Println("New Connection")
 	conn, err := wsUpgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -53,3 +51,23 @@ func (m* Manager) serverWS(w http.ResponseWriter, r *http.Request){
 	log.Println(conn)
 }
 
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.clients[client] = true
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	// check if client exists, if yes delete
+	if _, ok := m.clients[client]; ok {
+		//clean-up with closing connection
+		client.conn.Close()
+
+		// remove from active list
+		delete(m.clients, client)
+	}
+}
