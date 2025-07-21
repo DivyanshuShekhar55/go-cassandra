8dba77047aa51fdde9927bdbd0a4268059e911ae
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-07-21 19:12:15 +0530

    added missing listener

--- go/main.go
+++ go/main.go
@@ -12,13 +12,18 @@ func main() {
 	defer cancel()
 
 	manager := NewManager(ctx)
-	
-	// run the listen channel which keeps running 
-	// listens to any group message and sends the message 
-	// to online members of the group on that particular server 
+
+	// run the listen channel which keeps running
+	// listens to any group message and sends the message
+	// to online members of the group on that particular server
 	go ListenToChannel(broadcast, manager)
 
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 	http.HandleFunc("/ws", manager.serverWS)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 
 }
